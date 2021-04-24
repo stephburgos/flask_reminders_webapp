863113c1066da3dd054327022bc57361a73d8922
Your Name <typeyouremail@gmail.com>
2021-04-24 17:16:41 -0400

    Completed styling and edit and view all completed tasks functionality

--- app.py
+++ app.py
@@ -52,22 +52,45 @@ def edit(action, id):
         curs.execute("UPDATE todos SET status=(?) WHERE rowid=(?)", ('done',id))
         if(scheduler.get_job(str_id)):
             scheduler.remove_job(str_id)
+    elif action == 'edit':
+        if request.method == 'GET':
+            rows = curs.execute("SELECT rowid, todo, deadline FROM todos WHERE rowid=(?)", (id,))
+            #curs.execute("SELECT rowid, todo, deadline from todos WHERE status=(?) ORDER BY deadline ASC", ("pending",))
+            todo = {}
+            for row in rows:
+                todo = {'id': row[0], 'todo': row[1], 'deadline':row[2]}
+            return render_template('edit.html', todo=todo ) 
+        else: 
+            #get posted form data using names assigned in HTML
+            todo = request.form['reminder']
+            deadline = request.form['time']
+            curs.execute("UPDATE todos SET todo=(?), deadline=(?) WHERE rowid=(?)", (todo, deadline, id))
+            if(scheduler.get_job(str_id)):
+                scheduler.remove_job(str_id)
+        if(scheduler.get_job(str_id)):
+            scheduler.remove_job(str_id)
+            scheduler.add_job(id=str_id, func=give_reminder, trigger='date', run_date=deadline, args=[todo])
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
 
-@app.route('/all')
-def all():
-    todos = get_all_todos(curs, 'all') 
-    return render_template('index.html', to_dos = todos)    
+@app.route('/completed')
+def completed():
+     #connect to database
+    conn = sqlite3.connect('./static/data/toDos.db')
+    curs = conn.cursor()
+    todos = get_all_todos(curs, 'done') 
+    return render_template('all.html', todos = todos)    
 
 def get_all_todos(curs, status):
     todos = []
     rows = ''
     if status == 'all':
         rows = curs.execute("SELECT rowid, todo, deadline from todos ORDER BY deadline ASC")
-    else:
+    elif status == 'pending':
         rows = curs.execute("SELECT rowid, todo, deadline from todos WHERE status=(?) ORDER BY deadline ASC", ("pending",))
+    elif status == 'done':
+        rows = curs.execute("SELECT rowid, todo, deadline from todos WHERE status=(?) ORDER BY deadline ASC", ("done",))
     for row in rows:
         todo = {'id': row[0], 'todo': row[1], 'deadline':row[2]}
         todos.append(todo)
